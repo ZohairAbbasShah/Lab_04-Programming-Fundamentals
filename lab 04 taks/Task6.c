9ff731d36fe15ccbe0a92c696a1e2ecc613c7c64
ZohairAbbasShah <abbaszohair848@gmail.com>
2026-09-08 13:41:34 +0500

    Refactor Task6.c for clarity and correctness

--- lab 04 taks/Task6.c
+++ lab 04 taks/Task6.c
@@ -1,45 +1,46 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+
+int main() {
     int month;
     int year;
+
     printf("Enter the month: ");
-    scanf("%i",&month);
+    scanf("%i", &month);
+
     printf("Enter the year: ");
-    scanf("%i",&year);
-    switch (month){
+    scanf("%i", &year);
+
+    switch (month) {
+
         case 1:
         case 3:
         case 5:
         case 7:
         case 8:
         case 10:
-        case 12: 
-         printf("this month has 31 days");
-         break;
+        case 12:
+            printf("This month has 31 days");
+            break;
 
         case 4:
         case 6:
         case 9:
         case 11:
-         printf("this month has 30 days");
-         break;
+            printf("This month has 30 days");
+            break;
 
         case 2:
-           printf("enter the year: ");
-           scanf("%i",&year);
-           
-           if (year%4==0 && year%100!=0 || year%400==0){
-           printf(" february %i has 28 days",year);}
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                printf("February has 29 days");
+            }
+            else {
+                printf("February has 28 days");
+            }
+            break;
 
-           else{
-            printf("February %i has 29 days",year);
-           }
-           break;
-             
-		 default:
-         printf("invalid month");
-        
+        default:
+            printf("Invalid month");
     }
-    
-      return 0;
+
+    return 0;
 }
